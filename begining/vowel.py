3b253ffe8ca250f1556541569c9ae7b7a146cdb8
9177929096 <41770131+9177929096@users.noreply.github.com>
2018-07-27 15:44:53 +0530

    Create vowel.py

---
+++ begining/vowel.py
@@ -0,0 +1,13 @@
+n=raw_input()
+if(n=='a'):
+              print("vowel")
+elif(n=='e'):
+              print("vowel")
+elif(n=='i'):
+              print("vowel")
+elif(n=='o'):
+              print("vowel")
+elif(n=='u'):
+              print("vowel")
+else:
+              print("consonant")      
