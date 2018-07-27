e7c78b4121d4bae6ff5f1c1a0f285c9fe37d8ae2
9177929096 <41770131+9177929096@users.noreply.github.com>
2018-07-27 14:08:27 +0530

    Update positive.py

--- begining/positive.py
+++ begining/positive.py
@@ -1,7 +1,7 @@
 n=int(input())
 if(n>0):
-       print("Positve")
- elif(n<0):
-       print("Negative")
-  else:
-        print("Zero")
+	print("Positve")
+elif(n<0):
+	print("Negative")
+else:
+	print("Zero")
