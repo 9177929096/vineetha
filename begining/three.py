c25ea4eca24db4c6536054d211921fc8771ea5e5
9177929096 <41770131+9177929096@users.noreply.github.com>
2018-08-23 12:47:08 +0530

    Update three.py

--- begining/three.py
+++ begining/three.py
@@ -1,9 +1,9 @@
-n1,n2,n3=input().split()
+n1,n2,n3=map(int,input().split())
 if(n1<n2):
           if(n2<n3):
                      print(n3)
           else:
-                     print(n3)
+                     print(n2)
 else:
           if(n1<n3):
                      print(n3)
